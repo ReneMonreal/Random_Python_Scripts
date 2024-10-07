54faab6a7a55f29ba499b1df1ebbf2e69c39c348
Reennneee._ <139669566+ReneMonreal@users.noreply.github.com>
2024-10-07 16:20:47 -0500

    Calculator.py

--- Calculator .py
+++ Calculator .py
@@ -1,5 +1,6 @@
-#Created for practice
+#This is the first version i created when i was learning how to code in python, please view "Updated Calculator" for the new method.
 
+#Creating processes that run based on the answer
 def add(a, b):
     answer = a + b
     print(str(a) + " + " + str(b) + " = " + str(answer) + "\n")
@@ -15,16 +16,15 @@ def mul(a, b):
 def div(a, b):
     answer = a / b
     print(str(a) + " / " + str(b) + " = " + str(answer) + "\n")
-
+# Anserts that can be selected, more operators can be added but i will needed to create a new function for them
 while True:
     print("A. Addition")
     print("B. Subtraction")
     print("C. Multiplication")
     print("D. Division")
     print("E. Exit")
-
-    choice = input("input your choice:")
-
+    choice = input("Select what you would like to do: ")
+#This is what reads the the inputs.
     if choice == "a" or choice == "A":
         print("Addition")
         a = int(input("input first number:"))
